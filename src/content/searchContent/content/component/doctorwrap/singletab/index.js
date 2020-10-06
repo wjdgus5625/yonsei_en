f9685a0654b35084786549fa2c7f47d402d03b59
wjdgus5625 <wjdgus5625@naver.com>
2020-10-06 13:25:22 +0900

    진료과 탭의 구분코드 변수명 변경

--- src/content/searchContent/content/component/doctorwrap/singletab/index.js
+++ src/content/searchContent/content/component/doctorwrap/singletab/index.js
@@ -4,27 +4,27 @@ import { RootContext } from '../../../../..';
 const SingleTab = () => {
     const rootContext = useContext(RootContext);
 
-    const department = rootContext.request.department;
+    const gubun_cd = rootContext.request.gubun_cd;
 
-    const getSearchDepartment = (department) => {
+    const getSearchDepartment = (gubun_cd) => {
         rootContext.setRequest({
             ...rootContext.request,
-            department: department
+            gubun_cd: gubun_cd
         })
     }
 
     return (
         <div className="doctor-check">
             <span className="mr-5 ml-lg-7">
-                <input type="radio" name="radio1" id="radio1-1" className="custom-control" onChange={() => getSearchDepartment("진료과")} checked={department === undefined || department === "진료과" ? true : false} />
+                <input type="radio" name="radio1" id="radio1-1" className="custom-control" onChange={() => getSearchDepartment("진료과")} checked={gubun_cd === undefined || gubun_cd === "진료과" ? true : false} />
                 <label htmlFor="radio1-1">진료과</label>
             </span>
             <span className="mr-5">
-                <input type="radio" name="radio1" id="radio1-2" className="custom-control" onChange={() => getSearchDepartment("센터")} checked={department === "센터" ? true : false} />
+                <input type="radio" name="radio1" id="radio1-2" className="custom-control" onChange={() => getSearchDepartment("센터")} checked={gubun_cd === "센터" ? true : false} />
                 <label htmlFor="radio1-2">센터</label>
             </span>
             <span>
-                <input type="radio" name="radio1" id="radio1-3" className="custom-control" onChange={() => getSearchDepartment("클리닉")} checked={department === "클리닉" ? true : false} />
+                <input type="radio" name="radio1" id="radio1-3" className="custom-control" onChange={() => getSearchDepartment("클리닉")} checked={gubun_cd === "클리닉" ? true : false} />
                 <label htmlFor="radio1-3">클리닉</label>
             </span>
         </div>
