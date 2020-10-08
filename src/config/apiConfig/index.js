5aa641cfe774d61ce897561dcb06770ce974281d
wjdgus5625 <wjdgus5625@naver.com>
2020-10-08 12:27:00 +0900

    node module 재설치중

--- src/config/apiConfig/index.js
+++ src/config/apiConfig/index.js
@@ -1,8 +1,8 @@
 export default {
     ip: "10.9.32.17",
     port: 19700,
-    // search_path: "http://10.9.32.17:19700/search",
-    // pop_path: "http://10.9.32.17:19700/popkeyword"
-    search_path: "http://localhost:19700/search",
-    pop_path: "http://localhost:19700/popkeyword"
+    search_path: "http://10.9.32.17:19700/search",
+    pop_path: "http://10.9.32.17:19700/popkeyword"
+    // search_path: "http://localhost:19700/search",
+    // pop_path: "http://localhost:19700/popkeyword"
 }
