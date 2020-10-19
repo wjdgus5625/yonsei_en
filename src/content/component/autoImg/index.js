612714dcfbb7c06182d543fd2756cdbeda01d626
wjdgus5625 <wjdgus5625@naver.com>
2020-10-19 10:42:10 +0900

    폴더 구조 변경

---
+++ src/content/component/autoImg/index.js
@@ -0,0 +1,127 @@
+import React from 'react';
+
+const AutoImg = () => {
+    return (
+      <div className="related-search-wrap">
+        <div className="search-list bg-white pb-0">
+          <div
+            className="custom-scroll mCustomScrollbar _mCS_6"
+            style={{maxHeight: "130px"}}
+          >
+            <div id="mCSB_6" className="mCustomScrollBox mCS-light mCSB_vertical mCSB_inside"
+              tabndex="0"
+              style={{maxHeight: "130px"}}
+            >
+              <div
+                id="mCSB_6_container"
+                className="mCSB_container"
+                style={{position: "relative", top:0, left:0}}
+                dir="ltr"
+              >
+                <ul className="list mb-md-0">
+                  <li>
+                    <a href="http://pms.inseq.co.kr:9000/kth/yuhs/files/develop/sev/_guide/_template-header01.html#none">
+                      갑상선 기능저하증
+                    </a>
+                    <a
+                      href="http://pms.inseq.co.kr:9000/kth/yuhs/files/develop/sev/_guide/_template-header01.html#none"
+                      className="float-right"
+                    >
+                      <i className="ico ico-times-sm"></i>
+                      <span className="sr-only">삭제</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="http://pms.inseq.co.kr:9000/kth/yuhs/files/develop/sev/_guide/_template-header01.html#none">
+                      갑상선 기능저하증
+                    </a>
+                    <a
+                      href="http://pms.inseq.co.kr:9000/kth/yuhs/files/develop/sev/_guide/_template-header01.html#none"
+                      className="float-right"
+                    >
+                      <i className="ico ico-times-sm"></i>
+                      <span className="sr-only">삭제</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="http://pms.inseq.co.kr:9000/kth/yuhs/files/develop/sev/_guide/_template-header01.html#none">
+                      갑상선 기능저하증
+                    </a>
+                    <a
+                      href="http://pms.inseq.co.kr:9000/kth/yuhs/files/develop/sev/_guide/_template-header01.html#none"
+                      className="float-right"
+                    >
+                      <i className="ico ico-times-sm"></i>
+                      <span className="sr-only">삭제</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="http://pms.inseq.co.kr:9000/kth/yuhs/files/develop/sev/_guide/_template-header01.html#none">
+                      갑상선 기능저하증
+                    </a>
+                    <a
+                      href="http://pms.inseq.co.kr:9000/kth/yuhs/files/develop/sev/_guide/_template-header01.html#none"
+                      className="float-right"
+                    >
+                      <i className="ico ico-times-sm"></i>
+                      <span className="sr-only">삭제</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="http://pms.inseq.co.kr:9000/kth/yuhs/files/develop/sev/_guide/_template-header01.html#none">
+                      갑상선 기능저하증
+                    </a>
+                    <a
+                      href="http://pms.inseq.co.kr:9000/kth/yuhs/files/develop/sev/_guide/_template-header01.html#none"
+                      className="float-right"
+                    >
+                      <i className="ico ico-times-sm"></i>
+                      <span className="sr-only">삭제</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="http://pms.inseq.co.kr:9000/kth/yuhs/files/develop/sev/_guide/_template-header01.html#none">
+                      갑상선 기능저하증
+                    </a>
+                    <a
+                      href="http://pms.inseq.co.kr:9000/kth/yuhs/files/develop/sev/_guide/_template-header01.html#none"
+                      className="float-right"
+                    >
+                      <i className="ico ico-times-sm"></i>
+                      <span className="sr-only">삭제</span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div
+                id="mCSB_6_scrollbar_vertical"
+                className="mCSB_scrollTools mCSB_6_scrollbar mCS-light mCSB_scrollTools_vertical"
+                style={{display: "block"}}
+              >
+                <div className="mCSB_draggerContainer">
+                  <div
+                    id="mCSB_6_dragger_vertical"
+                    className="mCSB_dragger"
+                    style={{position: "absolute", minHeight: "30px", display: "block", height: "89px", maxHeight: "120px", top: "0px"}}
+                  >
+                    <div
+                      className="mCSB_dragger_bar"
+                      style={{lineHeight: "30px"}}
+                    ></div>
+                  </div>
+                  <div className="mCSB_draggerRail"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bg-whiteblue all-del text-center">
+            <a href="http://pms.inseq.co.kr:9000/kth/yuhs/files/develop/sev/_guide/_template-header01.html#none">
+              일괄 삭제
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default AutoImg;
---
+++ src/content/component/autoKeyword/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import parser from 'html-react-parser';
+
+const AutoKeyword = ({type, list, deleteRecentKeyword}) => {
+    return (
+        <div className="search-header related-search-wrap" style={{display: type !== "recentkeyword" && list.length === 0 ? "none" : ""}}>
+            <div className={type === "recentkeyword" ? "search-list bg-white pb-0" : "search-list bg-white"}>
+                <div className="custom-scroll" style={{maxHeight: type === "recentkeyword" ? "130px" : "150px"}}>
+                    <ul className={type === "recentkeyword" ? "list mb-md-0" : "list"}>
+                        {
+                            type === "recentkeyword" ? (
+                                list.length > 0 ? (
+                                    list.map((data, index) => {
+                                        return (
+                                            <li key={index}>
+                                                <a href="#;">{data}</a>
+                                                <a href="#;" className="float-right" onMouseDown={(e) => {
+                                                    e.preventDefault()
+                                                    deleteRecentKeyword(data)
+                                                }}>
+                                                    <i className="ico ico-times-sm"></i>
+                                                    <span className="sr-only">삭제</span>
+                                                </a>
+                                            </li>
+                                        )
+                                    })
+                                ) : (
+                                    <li>최근 검색어가 없습니다.</li>
+                                )
+                            ) : (
+                                list.map((data, index) => {
+                                    return (
+                                        <li key={index}><a href="#;">{parser(data)}</a></li>
+                                    )
+                                })
+                            )
+                        }
+                    </ul>
+                </div>
+                {
+                    type === "recentkeyword" ? (
+                        <div className="bg-whiteblue all-del text-center">
+                            <a href="#none;">일괄 삭제</a>
+                        </div>
+                    ) : ""
+                }
+            </div>
+        </div>
+    );
+}
+
+export default AutoKeyword;
