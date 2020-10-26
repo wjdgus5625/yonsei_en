70a3963f502518fced6836d76246db68a2cbaa21
wjdgus5625 <wjdgus5625@naver.com>
2020-10-26 16:23:34 +0900

    화면 설정 view properties 변경

--- src/config/searchViewSetting/index.js
+++ src/config/searchViewSetting/index.js
@@ -4,8 +4,6 @@ export default {
 		headquarter_kor: ["통합검색", "의료원", "뉴스", "채용", "기금", "연구"],
 		hospital: ["all", "department", "doctor", "health", "news", "about"],
 		hospital_kor: ["통합검색", "진료과", "의료진", "건강정보", "뉴스/소식", "병원안내"],
-		// hospital: ["all", "department", "doctor", "health", "seminar", "news", "guide", "about"],
-		// hospital_kor: ["통합검색", "진료과", "의료진", "건강정보", "세미나/강좌", "뉴스/소식", "이용안내", "병원소개"],
 		school: ["all", "professor", "news", "admission", "education", "about"],
 		school_kor: ["통합검색", "교수진", "뉴스", "입학", "교육/연구", "소개"]
 	},
