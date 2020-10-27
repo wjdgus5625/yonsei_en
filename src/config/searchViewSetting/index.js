32a2c0bba0da5a9f424809564f768e37602adfad
wjdgus5625 <wjdgus5625@naver.com>
2020-10-27 17:00:04 +0900

    병원소개 > 병원안내로 변경

--- src/config/searchViewSetting/index.js
+++ src/config/searchViewSetting/index.js
@@ -2,7 +2,7 @@ export default {
 	tablist: {
 		headquarter: ["all", "about", "news", "recruit", "fund", "research"],
 		headquarter_kor: ["통합검색", "의료원", "뉴스", "채용", "기금", "연구"],
-		hospital: ["all", "department", "doctor", "health", "news", "about"],
+		hospital: ["all", "department", "doctor", "health", "news", "guide"],
 		hospital_kor: ["통합검색", "진료과", "의료진", "건강정보", "뉴스/소식", "병원안내"],
 		school: ["all", "professor", "news", "admission", "education", "about", "link1", "link2"],
 		school_kor: ["통합검색", "교수진", "뉴스", "입학", "교육/연구", "소개", "논문", "연구자료"]
@@ -72,7 +72,7 @@ export default {
 				singletab: "select2"
 			},
 			guide: {
-				title: "이용안내",
+				title: "병원안내",
 				class: "mt-lg-12 mt-md-6",
 				singletab: "select2"
 			},
